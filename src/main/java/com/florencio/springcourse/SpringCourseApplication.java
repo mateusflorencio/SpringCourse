ac24adbb32751f2f2cb0bb98c0e5a4e0abd3226d
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-21 19:58:32 -0300

    Produto/repository criado e estanciado

--- src/main/java/com/florencio/springcourse/SpringCourseApplication.java
+++ src/main/java/com/florencio/springcourse/SpringCourseApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.florencio.springcourse.domain.Categoria;
+import com.florencio.springcourse.domain.Produto;
 import com.florencio.springcourse.repositories.CategoriaRepository;
+import com.florencio.springcourse.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SpringCourseApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
 	}
@@ -25,8 +29,21 @@ public class SpringCourseApplication implements CommandLineRunner {
 
 		Categoria c1 = new Categoria(null, "Informatica");
 		Categoria c2 = new Categoria(null, "Escritório");
-
+		
+		Produto p1= new Produto(null, "Computador", 2000.00);
+		Produto p2= new Produto(null, "Impressora", 800.00);
+		Produto p3= new Produto(null, "mouse", 80.00);
+	
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+	
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
