21aa6942f76aff421aaed8eaeabe004eb1d05d94
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-21 19:11:16 -0300

    Criando operação de instaciação

--- src/main/java/com/florencio/springcourse/SpringCourseApplication.java
+++ src/main/java/com/florencio/springcourse/SpringCourseApplication.java
@@ -1,13 +1,32 @@
 package com.florencio.springcourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.florencio.springcourse.domain.Categoria;
+import com.florencio.springcourse.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringCourseApplication {
+public class SpringCourseApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria c1 = new Categoria(null, "Informatica");
+		Categoria c2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(c1, c2));
 	}
 
 }
--- src/main/java/com/florencio/springcourse/domain/Categoria.java
+++ src/main/java/com/florencio/springcourse/domain/Categoria.java
@@ -15,23 +15,23 @@ public class Categoria implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Integer id;
 	private String nome;
 
 	public Categoria() {
 	}
 
-	public Categoria(int id, String nome) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -57,7 +57,9 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return id == other.id;
+		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
