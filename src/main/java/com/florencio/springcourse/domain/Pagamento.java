ebc0ab6e15d7c89118b12ac80a64d54190400eda
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-22 00:55:04 -0300

    Atualização jsonignore

--- src/main/java/com/florencio/springcourse/domain/Pagamento.java
+++ src/main/java/com/florencio/springcourse/domain/Pagamento.java
@@ -11,7 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.florencio.springcourse.domain.enums.EstadoPagamento;
 
 @Entity
@@ -26,7 +26,7 @@ public abstract class Pagamento implements Serializable {
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId //para garantir que o mesmo id do pedido é o id do pagamento
-	@JsonBackReference
+	@JsonIgnore
 	private Pedido pedido;
 	
 	public Pagamento () {
--- src/main/java/com/florencio/springcourse/domain/Pedido.java
+++ src/main/java/com/florencio/springcourse/domain/Pedido.java
@@ -17,7 +17,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -26,24 +25,23 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	@JsonFormat (pattern = "dd/MM/yyyy HH:mm")
+
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
-	
-	@JsonManagedReference
+
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido") // para evitar conflito no "id" (peculiaridade da jpa)
 	private Pagamento pagamento;
 
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
-	@JsonManagedReference
+
 	private Cliente cliente;
 
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-	
-	@OneToMany (mappedBy = "id.pedido") //utilizar o id. para acessa a classe pk(emblebad)
+
+	@OneToMany(mappedBy = "id.pedido") // utilizar o id. para acessa a classe pk(emblebad)
 	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
