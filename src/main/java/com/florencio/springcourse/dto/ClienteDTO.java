7db0590c21beb056765a6c964ff099054237ed6e
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-22 17:43:08 -0300

    email não repetido

--- src/main/java/com/florencio/springcourse/dto/ClienteDTO.java
+++ src/main/java/com/florencio/springcourse/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.florencio.springcourse.domain.Cliente;
+import com.florencio.springcourse.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/com/florencio/springcourse/repositories/ClienteRepository.java
+++ src/main/java/com/florencio/springcourse/repositories/ClienteRepository.java
@@ -1,15 +1,14 @@
 package com.florencio.springcourse.repositories;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.florencio.springcourse.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-	@Transactional
+	@Transactional(readOnly=true)
 	Cliente findByEmail(String email);
 }
