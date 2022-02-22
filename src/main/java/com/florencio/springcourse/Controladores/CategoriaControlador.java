9e685cd855da81100e482d52d8e1da0f1eab6e62
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-22 10:05:11 -0300

    Atualizando categoria com put

--- src/main/java/com/florencio/springcourse/Controladores/CategoriaControlador.java
+++ src/main/java/com/florencio/springcourse/Controladores/CategoriaControlador.java
@@ -22,7 +22,7 @@ public class CategoriaControlador{
 	private CategoriaService service;
 
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -33,5 +33,14 @@ public class CategoriaControlador{
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id,@RequestBody Categoria obj){
+		obj.setId(id);
+		service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 }
