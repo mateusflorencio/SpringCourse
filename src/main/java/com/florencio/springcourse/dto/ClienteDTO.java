de68244797cc6c574b3d7ab28fa4fa2602112a75
Mateus Florencio <mateusflorencio@outlook.com.br>
2022-02-22 14:32:42 -0300

    put, delet e insert cliente

---
+++ src/main/java/com/florencio/springcourse/dto/ClienteDTO.java
@@ -0,0 +1,56 @@
+package com.florencio.springcourse.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.florencio.springcourse.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	@NotEmpty
+	@Size(min = 5, max = 120, message = "Deve haver a inserção de 5 a 120 caracteres")
+	private String nome;
+	@NotEmpty
+	@Email (message = "inserir um email adequado")
+	private String email;
+
+	public ClienteDTO() {
+
+	}
+
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
