ced8f9a80633a0378809630f2a2f404c5f058a8a
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-02-23 21:32:42 -0300

    Criando profile dev

---
+++ src/main/java/com/florencio/springcourse/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.florencio.springcourse.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.florencio.springcourse.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	public DBService dbService;
+	
+	@Value ("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
