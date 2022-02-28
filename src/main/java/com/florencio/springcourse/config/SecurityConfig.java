4c14c3ac85bc19ff686b401d667187a313e83b9a
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-02-28 14:33:30 -0300

    implementando autorizacao

--- src/main/java/com/florencio/springcourse/config/SecurityConfig.java
+++ src/main/java/com/florencio/springcourse/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.florencio.springcourse.security.JWTAuthenticationFilter;
+import com.florencio.springcourse.security.JWTAuthorizationFilter;
 import com.florencio.springcourse.security.JWTUtil;
 
 @Configuration
@@ -27,46 +28,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-
+	
 	@Autowired
-	private Environment env;
-
+    private Environment env;
+	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
 
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
-
-	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes/**"
+	};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable(); // If utilizado para acessar o profile de teste sem erro
-		}
-
+            http.headers().frameOptions().disable();
+        }
+		
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // só permiti metodo get
-				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-
+	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
 }
