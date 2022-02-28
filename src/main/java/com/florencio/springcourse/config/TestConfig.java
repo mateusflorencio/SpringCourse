95d6f9ee3ed01bbfb939bc65767a10e657f689fa
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-02-28 11:34:10 -0300

    Configuração inicial so Spring Security

--- src/main/java/com/florencio/springcourse/config/TestConfig.java
+++ src/main/java/com/florencio/springcourse/config/TestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.florencio.springcourse.services.DBService;
 import com.florencio.springcourse.services.EmailService;
+import com.florencio.springcourse.services.MockEmailService;
 import com.florencio.springcourse.services.SmtpEmailService;
 
 @Configuration
@@ -27,7 +28,7 @@ public class TestConfig {
 	
 @Bean
 	public EmailService emailService(){
-		return new SmtpEmailService();
+		return new MockEmailService();
 	}
 
 }
