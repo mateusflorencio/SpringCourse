78bccf0b434b7a3614faf461ec019fe8f61e4792
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-03-02 17:03:02 -0300

    tratando excessoes adequadamente

--- src/main/java/com/florencio/springcourse/Controladores/exception/ManipuladorDeExcecoesDoControlador.java
+++ src/main/java/com/florencio/springcourse/Controladores/exception/ManipuladorDeExcecoesDoControlador.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.florencio.springcourse.services.exceptions.AuthorizationException;
 import com.florencio.springcourse.services.exceptions.DataIntegrityException;
+import com.florencio.springcourse.services.exceptions.FileException;
 import com.florencio.springcourse.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -40,10 +44,37 @@ public class ManipuladorDeExcecoesDoControlador {
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
-	
+
 	@ExceptionHandler(AuthorizationException.class) // erro desse tipo de objeto
 	public ResponseEntity<ErrosPadrao> authorization(ObjectNotFoundException e, HttpServletRequest request) {
 		ErrosPadrao err = new ErrosPadrao(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	}
+
+	@ExceptionHandler(FileException.class) // erro desse tipo de objeto
+	public ResponseEntity<ErrosPadrao> file(FileException e, HttpServletRequest request) {
+		ErrosPadrao err = new ErrosPadrao(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
+	@ExceptionHandler(AmazonServiceException.class) // erro desse tipo de objeto
+	public ResponseEntity<ErrosPadrao> amazonService(AmazonServiceException e, HttpServletRequest request) {
+		
+		HttpStatus code = HttpStatus.valueOf(e.getErrorCode()); // pega o erro do http
+		ErrosPadrao err = new ErrosPadrao(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code).body(err);
+	}
+	
+	@ExceptionHandler(AmazonClientException.class) // erro desse tipo de objeto
+	public ResponseEntity<ErrosPadrao> amazonClient(AmazonClientException e, HttpServletRequest request) {
+		ErrosPadrao err = new ErrosPadrao(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class) // erro desse tipo de objeto
+	public ResponseEntity<ErrosPadrao> amazonS3 (AmazonS3Exception e, HttpServletRequest request) {
+		ErrosPadrao err = new ErrosPadrao(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
 }
